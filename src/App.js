db7a10efeeff57ffd28fc471766dcfca5f1d7050
yigitozay <yigit.ozay344@gmail.com>
2024-02-16 04:41:05 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Cardlist from "./Cardlist";
+import SearchBox from "./SearchBox";
+import './App.css'
+import Scroll from './Scroll'
+class App extends Component {
+    constructor(){
+        super()
+        this.state = {
+            robots: [],
+            searchField: ''
+        }
+    }
+    componentDidMount(){
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(users => {this.setState({robots: users})})
+    }
+    onSearchChange = (event) => {
+        this.setState({ searchField: event.target.value})
+    }
+    render(){
+        const filteredRobots = this.state.robots.filter(robots => {
+            return robots.name.toLowerCase().includes(this.state.searchField.toLowerCase())
+        })
+        return (
+            <div className="tc">
+            <h1>RoboFriends</h1>
+            <SearchBox searchChange = {this.onSearchChange}/>
+            <Scroll>
+            <Cardlist robots = {filteredRobots}/>
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+            </Scroll>
+            </div>
+        );
+    }
+    
 }
+
 
 export default App;
